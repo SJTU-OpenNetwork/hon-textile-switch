55c4a6e4c5c541237b3343f83a690b07d77c56cf
Riften <943275366@qq.com>
2020-04-27 10:16:36 +0800

    add request stream

--- core/stream.go
+++ core/stream.go
@@ -1,8 +1,16 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/SJTU-OpenNetwork/hon-textile-switch/pb"
+)
 
 func (t *Textile) SubscribeStream(streamId string) error {
 	fmt.Printf("Shadow peer should not call subscribe!!\nUse request directly\n")
 	return nil
+}
+
+// RequestStream request a stream from a provider by sending stream request.
+func (t* Textile) RequestStream(pid string, config *pb.StreamRequest) (*pb.Envelope, error){
+	return t.stream.SendStreamRequest(pid, config)
 }
